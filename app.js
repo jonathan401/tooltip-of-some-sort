d24f4ec0cc504c37b149217023c4f3f9674ebc9e
jonathan401 <jonathangomina5049@gmail.com>
2022-07-25 02:38:54 +0100

    add buttons

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const toolbar = document.querySelector(".toolbar");
 const avatar = document.querySelector(".avatar");
+const button = document.querySelector("button");
+
+// change toolbar direction
+button.addEventListener("click", () => {
+  toolbar.classList.toggle("from-left");
+});
 
 // toggle toolbar
 const toogleToolbar = () => {
+  // console.log(avatar.offsetWidth);
   toolbar.classList.toggle("open");
 };
 
