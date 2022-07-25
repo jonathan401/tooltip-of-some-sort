13820518a54a8370420610ca8c9408c3e7c13d25
jonathan401 <jonathangomina5049@gmail.com>
2022-07-26 00:17:49 +0100

    provide little bit of customization to toolbars

--- public/toolbar.js
+++ public/toolbar.js
@@ -1,2 +1,59 @@
 "use strict";
-console.log('test');
+var avatar = document.querySelector('.avatar');
+var box = document.querySelector('.box');
+var box2 = document.querySelector('.box2');
+// toolbar class
+var Toolbar = /** @class */ (function () {
+    function Toolbar(container, width, height, color, position) {
+        var _this = this;
+        this.setPosition = function (toolbar) {
+            switch (_this.position) {
+                case 'right-bottom':
+                    toolbar.style.transformOrigin = 'left top';
+                    toolbar.style.left = "".concat((_this.container.offsetWidth / 2));
+                    break;
+                case 'left-bottom':
+                    toolbar.style.transformOrigin = 'right top';
+                    toolbar.style.right = "".concat((_this.container.offsetWidth / 2));
+                    break;
+                case 'center':
+                    toolbar.style.transformOrigin = 'top center';
+                    toolbar.style.right = "".concat((-1 * toolbar.offsetWidth / 2) + (_this.container.offsetWidth / 2));
+                    break;
+                default:
+                    toolbar.style.transformOrigin = 'top right';
+                    toolbar.style.right = "".concat((_this.container.offsetWidth / 2));
+                    break;
+            }
+            ;
+        };
+        this.init = function () {
+            var toolbar = document.createElement('div');
+            // add structure and style to toolbar
+            toolbar.style.background = _this.color ? "".concat(_this.color) : '#888';
+            toolbar.style.width = "".concat(_this.width, "px");
+            toolbar.style.height = "".concat(_this.height, "px");
+            toolbar.classList.add('toolbar');
+            // append the toolbar to the container
+            _this.container.appendChild(toolbar);
+            _this.setPosition(toolbar);
+            _this.container.addEventListener('click', function () {
+                toolbar.classList.toggle('open');
+            });
+        };
+        this.container = container;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.position = position;
+    }
+    return Toolbar;
+}());
+;
+// testing the class
+var avatarToolBar = new Toolbar(avatar, 90, 40, '#888', 'left-bottom');
+var boxToolBar = new Toolbar(box, 90, 40, '#666', 'center');
+var box2ToolBar = new Toolbar(box2, 90, 40, '#444', 'right-bottom');
+avatarToolBar.init();
+boxToolBar.init();
+box2ToolBar.init();
--- src/toolbar.ts
+++ src/toolbar.ts
@@ -1,1 +1,75 @@
-console.log('test');
+const avatar = document.querySelector('.avatar') as HTMLDivElement;
+const box = document.querySelector('.box') as HTMLDivElement;
+const box2 = document.querySelector('.box2') as HTMLDivElement;
+
+// toolbar class
+class Toolbar {
+  container: HTMLElement;
+  width: number;
+  height: number;
+  color?: string;
+  position?: 'right-bottom' | 'left-bottom' | 'center';
+
+  constructor (
+      container: HTMLElement, 
+      width: number, 
+      height: number, 
+      color?: string, 
+      position?: 'right-bottom' | 'left-bottom' | 'center',
+    ) {
+    this.container = container;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.position = position;
+  }
+
+  setPosition = (toolbar: HTMLDivElement) => {
+    switch(this.position) {
+      case 'right-bottom': 
+        toolbar.style.transformOrigin = 'left top';
+        toolbar.style.left = `${(this.container.offsetWidth / 2)}`;
+        break;
+
+      case 'left-bottom':
+        toolbar.style.transformOrigin = 'right top';
+        toolbar.style.right = `${(this.container.offsetWidth / 2)}`;
+        break;
+
+      case 'center':
+        toolbar.style.transformOrigin = 'top center';
+        toolbar.style.right = `${(-1 * toolbar.offsetWidth / 2) + (this.container.offsetWidth / 2)}`;
+        break;
+
+      default:
+        toolbar.style.transformOrigin = 'top right';
+        toolbar.style.right = `${(this.container.offsetWidth / 2)}`;    
+        break;
+      };
+  };
+
+  init = () => {
+    const toolbar = document.createElement('div');
+    // add structure and style to toolbar
+    toolbar.style.background = this.color ? `${this.color}`: '#888';
+    toolbar.style.width = `${this.width}px`;
+    toolbar.style.height = `${this.height}px`;
+    toolbar.classList.add('toolbar');    
+    
+    // append the toolbar to the container
+    this.container.appendChild(toolbar);
+    this.setPosition(toolbar);
+    
+    this.container.addEventListener('click', () => {
+      toolbar.classList.toggle('open');
+    });
+  };
+};
+
+// testing the class
+const avatarToolBar = new Toolbar(avatar, 90, 40, '#888', 'left-bottom');
+const boxToolBar = new Toolbar(box, 90, 40, '#666', 'center');
+const box2ToolBar = new Toolbar(box2, 90, 40, '#444', 'right-bottom');
+avatarToolBar.init();
+boxToolBar.init();
+box2ToolBar.init();
